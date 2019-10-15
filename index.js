904b21914340d61bc01c77f01ff6de4cf492c8bd
lia284 <56201109+lia284@users.noreply.github.com>
2019-10-15 16:22:44 +0200

    Update index.js

--- index.js
+++ index.js
@@ -70,6 +70,17 @@ class Game extends React.Component {
     const history = this.state.history;
     const current = history[history.length - 1];
     const winner = calculateWinner(current.squares);
+    
+    const moves = history.map((step, move) => {
+      const desc = move ? 
+            'go to move #' + move :
+            'Go to game start';
+      return (
+        <li>
+          <button onClick={() => this.jumpTo(move)}>{desc}</botton>
+        </li>
+      )
+    });
 
     let status;
     if (winner) {
@@ -88,7 +99,7 @@ class Game extends React.Component {
         </div>
         <div className="game-info">
           <div>{status}</div>
-          <ol>{/* TODO */}</ol>
+          <ol>{moves}</ol>
         </div>
       </div>
     );
